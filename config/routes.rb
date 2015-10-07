d40f83c13c436aff63c487140c32a8d2758d4c6c
kevinster11 <kevinsternberg@Kevins-MacBook-Pro.local>
2015-10-06 21:13:11 -0600

    Added the different pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 root "pages#index"
 get "/about" => "pages#about", as: :about
+get "/activies" => "pages#activies", as: :activies
+get "/donate" => "pages#donate", as: :donate
+get "/parents" => "pages#parents", as: :parents
+get "/staff" => "pages#staff", as: :staff
 end
